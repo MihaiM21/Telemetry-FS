48c4afb72a62fe4bffcdfa1d251431a1ed8736fe
MihaiM21 <mihai.marinescu2112@yahoo.com>
2024-02-09 17:55:12 +0200

    Client and server edit

--- Server/server.py
+++ Server/server.py
@@ -7,19 +7,19 @@ s.listen(1) #listening every 1 second
 print('Server started')
 
 data = "hau"
-
-def background_controller():
+user_input = ''
+def background_controller(userInput):
     print("Sending: ")
     print(data)
     clientsocket.send(bytes(data, 'utf-8'))
     Timer(1, background_controller).start()
 
-Flag = True
+    userInput = input("Enter something to send or 'q' to quit: ")
 
-while Flag:
+
+while True:
     clientsocket, address = s.accept()
     print(f'Connection from {address} has been established.')
-    background_controller()
-    User_inp = input()
-    if User_inp != '':
-        Flag = False
+    background_controller(user_input)
+    if user_input != '':
+        break
