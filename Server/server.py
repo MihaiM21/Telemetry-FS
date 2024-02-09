52bb98a8c05eb21eb17803eab2cb945318d26cdd
MihaiM21 <mihai.marinescu2112@yahoo.com>
2024-02-09 18:08:18 +0200

    Client and server edit

--- Server/server.py
+++ Server/server.py
@@ -3,21 +3,40 @@ from can import CanSocket
 import socket
 from threading import Thread, Timer
 
+#Server Initializing
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 5000))
 s.listen(1) #listening every 1 second
 print('Server started')
 
-data = "hau"
-user_input = ''
-def background_controller(userInput):
-    print("Sending: ")
-    print(data)
-    clientsocket.send(bytes(data, 'utf-8'))
-    Timer(1, background_controller).start()
+#CAN interface
+can_socket = CanSocket("can0")#can interface name
+
+def canConnect():
+    try:
+        can_socket.bind()
+    except socket.error as e:
+        print(f"Error binding to Can interface: {e}")
+        exit(1)
+
+def sendData():
+    while True:
+        try:
+            message = can_socket.recv()
+            print(f"Message received: {message}")
+            print(f"Sending message: {message}")
+        except socket.error as e:
+            print(f"Error receiving CAN message: {e}")
+
+        #sending data
+        clientsocket, address = s.accept()
+        print(f'Connection from {address} has been established.')
+        clientsocket.send(bytes(message, "utf-8"))
+
+#def background_controller(userInput):
+ #   print("Sending: ")
+  #  print('a')
+  #  clientsocket.send(bytes('a', 'utf-8'))
+   # Timer(1, background_controller).start()
 
 
-while True:
-    clientsocket, address = s.accept()
-    print(f'Connection from {address} has been established.')
-    background_controller(user_input)
